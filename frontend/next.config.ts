491265f83de81c35b7ae1cf45daa10df3c1d066f
josearlindoj <josearlindoj@gmail.com>
2024-11-10 09:25:26 -0300

    feat: updating cors policies

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,19 @@ const nextConfig: NextConfig = {
     images: {
         domains: ['localhost'],
     },
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    { key: 'Access-Control-Allow-Origin', value: 'https://cupcake-store-a1d66243d848.herokuapp.com' },
+                    { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, OPTIONS' },
+                    { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+                    { key: 'Access-Control-Allow-Credentials', value: 'true' },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
